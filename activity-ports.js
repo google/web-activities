f29dabab90b0033a5f43f348dde096be8fb3be04
dparikh <dipak.parikh@gmail.com>
2018-02-12 09:54:52 -0800

    Release v1.2.1

--- activity-ports.js
+++ activity-ports.js
@@ -14,9 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- /** Version: 1.2.0 */
+ /** Version: 1.2.1 */
 'use strict';
 
+/*eslint no-unused-vars: 0*/
 
 
 /**
@@ -151,7 +152,6 @@ class ActivityPort {
 
 const SENTINEL = '__ACTIVITIES__';
 
-
 /**
  * The messenger helper for activity's port and host.
  */
@@ -183,6 +183,11 @@ class Messenger {
     /** @private {?function(!Object)} */
     this.onCustomMessage_ = null;
 
+    /**
+     * @private {?Object<string, !ChannelHolder>}
+     */
+    this.channels_ = null;
+
     /** @private @const */
     this.boundHandleEvent_ = this.handleEvent_.bind(this);
   }
@@ -206,6 +211,18 @@ class Messenger {
     if (this.onCommand_) {
       this.onCommand_ = null;
       this.win_.removeEventListener('message', this.boundHandleEvent_);
+      if (this.channels_) {
+        for (const k in this.channels_) {
+          const channelObj = this.channels_[k];
+          if (channelObj.port1) {
+            closePort(channelObj.port1);
+          }
+          if (channelObj.port2) {
+            closePort(channelObj.port2);
+          }
+        }
+        this.channels_ = null;
+      }
     }
   }
 
@@ -261,8 +278,9 @@ class Messenger {
    * Sends the specified command from the port to the host or vice versa.
    * @param {string} cmd
    * @param {?Object=} opt_payload
+   * @param {?Array=} opt_transfer
    */
-  sendCommand(cmd, opt_payload) {
+  sendCommand(cmd, opt_payload, opt_transfer) {
     const target = this.getTarget();
     // Only "connect" command is allowed to use `targetOrigin == '*'`
     const targetOrigin =
@@ -273,7 +291,7 @@ class Messenger {
       'sentinel': SENTINEL,
       'cmd': cmd,
       'payload': opt_payload || null,
-    }, targetOrigin);
+    }, targetOrigin, opt_transfer || undefined);
   }
 
   /**
@@ -290,6 +308,76 @@ class Messenger {
    */
   onCustomMessage(callback) {
     this.onCustomMessage_ = callback;
+  }
+
+  /**
+   * @param {string=} opt_name
+   * @return {!Promise<!MessagePort>}
+   */
+  startChannel(opt_name) {
+    const name = opt_name || '';
+    const channelObj = this.getChannelObj_(name);
+    if (!channelObj.port1) {
+      const channel = new this.win_.MessageChannel();
+      channelObj.port1 = channel.port1;
+      channelObj.port2 = channel.port2;
+      channelObj.resolver(channelObj.port1);
+    }
+    if (channelObj.port2) {
+      // Not yet sent.
+      this.sendCommand('cnset', {'name': name}, [channelObj.port2]);
+      channelObj.port2 = null;
+    }
+    return channelObj.promise;
+  }
+
+  /**
+   * @param {string=} opt_name
+   * @return {!Promise<!MessagePort>}
+   */
+  askChannel(opt_name) {
+    const name = opt_name || '';
+    const channelObj = this.getChannelObj_(name);
+    if (!channelObj.port1) {
+      this.sendCommand('cnget', {'name': name});
+    }
+    return channelObj.promise;
+  }
+
+  /**
+   * @param {string} name
+   * @param {!MessagePort} port
+   * @private
+   */
+  receiveChannel_(name, port) {
+    const channelObj = this.getChannelObj_(name);
+    channelObj.port1 = port;
+    channelObj.resolver(port);
+  }
+
+  /**
+   * @param {string} name
+   * @return {!ChannelHolder}
+   */
+  getChannelObj_(name) {
+    if (!this.channels_) {
+      this.channels_ = {};
+    }
+    let channelObj = this.channels_[name];
+    if (!channelObj) {
+      let resolver;
+      const promise = new Promise(resolve => {
+        resolver = resolve;
+      });
+      channelObj = {
+        port1: null,
+        port2: null,
+        resolver,
+        promise,
+      };
+      this.channels_[name] = channelObj;
+    }
+    return channelObj;
   }
 
   /**
@@ -317,22 +405,42 @@ class Messenger {
     if (origin != this.targetOrigin_) {
       return;
     }
-    this.handleCommand_(cmd, payload);
+    this.handleCommand_(cmd, payload, event);
   }
 
   /**
    * @param {string} cmd
    * @param {?Object} payload
+   * @param {!MessageEvent} event
    * @private
    */
-  handleCommand_(cmd, payload) {
+  handleCommand_(cmd, payload, event) {
     if (cmd == 'msg') {
       if (this.onCustomMessage_ != null && payload != null) {
         this.onCustomMessage_(payload);
       }
+    } else if (cmd == 'cnget') {
+      const name = payload['name'];
+      this.startChannel(name);
+    } else if (cmd == 'cnset') {
+      const name = payload['name'];
+      const port = event.ports[0];
+      this.receiveChannel_(name, /** @type {!MessagePort} */ (port));
     } else {
       this.onCommand_(cmd, payload);
     }
+  }
+}
+
+
+/**
+ * @param {!MessagePort} port
+ */
+function closePort(port) {
+  try {
+    port.close();
+  } catch (e) {
+    // Ignore.
   }
 }
 
@@ -670,6 +778,15 @@ class ActivityIframePort {
    */
   onMessage(callback) {
     this.messenger_.onCustomMessage(callback);
+  }
+
+  /**
+   * Creates a new communication channel or returns an existing one.
+   * @param {string=} opt_name
+   * @return {!Promise<!MessagePort>}
+   */
+  messageChannel(opt_name) {
+    return this.messenger_.askChannel(opt_name);
   }
 
   /**
@@ -1155,7 +1272,7 @@ class ActivityPorts {
    */
   constructor(win) {
     /** @const {string} */
-    this.version = '1.2.0';
+    this.version = '1.2.1';
 
     /** @private @const {!Window} */
     this.win_ = win;
